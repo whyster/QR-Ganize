450a4ecf81ccd0f23c57f96903e96aa3c8d9f096
Aleksey Kalinovski <aleksey.kalinovski@gmail.com>
2023-11-26 04:37:56 -0500

    Refactored to use Result type

--- QR-Ganize-Lib/StorageManager.cs
+++ QR-Ganize-Lib/StorageManager.cs
@@ -3,6 +3,13 @@ using Microsoft.Extensions.Logging;
 
 namespace QR_Ganize_Lib;
 
+public enum CreationError
+{
+    AlreadyExists,
+    BoxNotFound,
+    LocationNotFound,
+}
+
 public class StorageManager
 {
     private StorageDbContext _dbContext;
@@ -20,7 +27,7 @@ public class StorageManager
         var duplicateItemTagMaps = _dbContext.ItemTagMap
             .GroupBy(itm => new { itm.ItemId, itm.TagId })
             .Where(g => g.Count() > 1);
-        
+
         foreach (var group in duplicateItemTagMaps)
         {
             // Skip the first record and remove the rest of duplicates in each group
@@ -53,121 +60,105 @@ public class StorageManager
         await _dbContext.SaveChangesAsync();
     }*/
 
-    public async Task<Item> CreateItem(string itemName, IEnumerable<Int32> tagIds, Int32 boxId)
+    public async Task<Result<Nothing, CreationError>> CreateItem(string itemName, IEnumerable<Int32> tagIds,
+        Int32 boxId)
     {
         // Check if the box exists
-        var existingBox = await _dbContext.Boxes.Include(box => box.Tags).ThenInclude(map => map.Tag).Include(box => box.Location).FirstAsync(box => box.BoxId == boxId );
+        var existingBox = await _dbContext.Boxes
+            .FirstOrDefaultAsync(box => box.BoxId == boxId);
 
-        // Throw an error if the box doesn't exist
-        if (existingBox == null)
-        {
-            throw new KeyNotFoundException($"Box with ID {boxId} does not exist");
-        }
-
+        // Return an error if the box doesn't exist
+        if (existingBox is null)
+            return new Err<Nothing, CreationError>(CreationError.BoxNotFound);
+        
         // Check if item with the same name and box already exists
-        var existingItem = await _dbContext.Items.Include(item => item.Tags).ThenInclude(item_map => item_map.Tag).Include(item => item.Box).ThenInclude(box => box.Tags).FirstOrDefaultAsync(item => item.Name == itemName && item.Box.BoxId == boxId);
+        var existingItem =
+            await _dbContext.Items.FirstOrDefaultAsync(item => item.Name == itemName && item.Box.BoxId == boxId);
 
         // Return the existing item if one was found
         if (existingItem != null)
-        {
-            _logger.LogInformation("Returning existing item");
-            return existingItem;
-        }
-            
+            return new Err<Nothing, CreationError>(CreationError.AlreadyExists);
+
         // Create a new item
-        var item = new Item { Name = itemName, Box = existingBox };
+        var item = new Item {Name = itemName, Box = existingBox};
 
         // Fetch all tags
         var existingTags = await _dbContext.Tags.Where(tag => tagIds.Contains(tag.TagId)).ToListAsync();
         // await DeDuplicateItemTagMaps();
 
         // Create ItemTagMap for each tag
-        var tagMaps = existingTags.Select(tag => new ItemTagMap { Item = item, Tag = tag });
+        var tagMaps = existingTags.Select(tag => new ItemTagMap {Item = item, Tag = tag});
 
         // Set item tags
         item.Tags = tagMaps.ToList();
-        
+
         // Add the item to database and save changes
         await _dbContext.Items.AddAsync(item);
         await _dbContext.SaveChangesAsync();
 
-        
-        _logger.LogInformation("Created Item {Item}", item.Name);
-        return item;
+
+        _logger.LogInformation("Created Item {@Item}",  new {ID=item.ItemId, Name=item.Name});
+        return new Ok<Nothing, CreationError>(Nothing.AtAll);
     }
-    public async Task<Box> CreateBox(string boxName, IEnumerable<Int32> tagIds, Int32 location)
+
+    public async Task<Result<Nothing, CreationError>> CreateBox(string boxName, IEnumerable<Int32> tagIds, Int32 location)
     {
 
         var existingLocation = await _dbContext.Locations.FindAsync(location);
         if (existingLocation == null)
         {
-            throw new KeyNotFoundException($"Location with ID {location} does not exist");
+            return new Err<Nothing, CreationError>(CreationError.LocationNotFound);
         }
-        var existingBox = await _dbContext.Boxes.Include(box => box.Tags).FirstOrDefaultAsync(box => box.Name == boxName && box.Location==existingLocation);
+
+        var existingBox = await _dbContext.Boxes
+            .FirstOrDefaultAsync(box => box.Name == boxName && box.Location == existingLocation);
 
         if (existingBox != null)
         {
-            // If item exists, return that item instead of creating a new one
-            return existingBox;
+            return new Err<Nothing, CreationError>(CreationError.AlreadyExists);
         }
-        
 
-        // var existingLocation = await _dbContext.Locations.FindAsync(location.LocationId) ?? location;
 
-        var existingTags =  await _dbContext.Tags.Where(tag => tagIds.Contains(tag.TagId)).ToListAsync();
+
+        var existingTags = await _dbContext.Tags.Where(tag => tagIds.Contains(tag.TagId)).ToListAsync();
 
         var box = new Box {Name = boxName, Location = existingLocation};
-        
+
         // TODO: make sure no duplicate boxTagMaps appear
         var tagMaps = existingTags.Select(tag => new BoxTagMap {Box = box, Tag = tag});
         // await DeDuplicateBoxTagMaps();
-        
-        
-        
+
+
         box.Tags = tagMaps.ToList();
-        
+
         await _dbContext.Boxes.AddAsync(box);
         await _dbContext.SaveChangesAsync();
-        return box;
+        return new Ok<Nothing, CreationError>(Nothing.AtAll);
     }
 
-    public async Task<Location> CreateLocation(string locationName)
+    public async Task<Result<Nothing, CreationError>> CreateLocation(string locationName)
     {
-        // Check if tag already exists in the database
-        var existingTag = await _dbContext.Locations.FirstOrDefaultAsync(location => location.Name == locationName);
+        var existingLocation = await _dbContext.Locations.FirstOrDefaultAsync(location => location.Name == locationName);
 
-        if (existingTag != null)
+        if (existingLocation != null)
         {
-            // If tag exists, return that tag instead of creating a new one
-            return existingTag;
+            return new Err<Nothing, CreationError>(CreationError.AlreadyExists);
         }
-
-        // If tag doesn't exist, then create a new tag
-
+        
         Location location = new Location {Name = locationName};
         await _dbContext.Locations.AddAsync(location);
         await _dbContext.SaveChangesAsync();
 
-        return location; // Return the newly created tag
+        return new Ok<Nothing, CreationError>(Nothing.AtAll);
     }
-    public async Task<IEnumerable<Tag>> GetTags(IEnumerable<string> tagNames)
-    {
-        if(!tagNames.Any())
-        {
-            return await _dbContext.Tags.ToListAsync();
-        }
 
-        return await _dbContext.Tags.Where(tag => tagNames.Contains(tag.Name)).ToListAsync();
-    }
-    public async Task<Tag> CreateTag(string tagName)
+    public async Task<Result<Nothing, CreationError>> CreateTag(string tagName)
     {
-        // Check if tag already exists in the database
         var existingTag = await _dbContext.Tags.FirstOrDefaultAsync(tag => tag.Name == tagName);
 
         if (existingTag != null)
         {
-            // If tag exists, return that tag instead of creating a new one
-            return existingTag;
+            return new Err<Nothing, CreationError>(CreationError.AlreadyExists);
         }
 
         // If tag doesn't exist, then create a new tag
@@ -176,6 +167,13 @@ public class StorageManager
         await _dbContext.Tags.AddAsync(tag);
         await _dbContext.SaveChangesAsync();
 
-        return tag; // Return the newly created tag    }
+        return new Ok<Nothing, CreationError>(Nothing.AtAll); 
+    }
+    
+    public async Task<IEnumerable<Tag>> GetTags(IEnumerable<string> tagNames)
+    {
+        if (!tagNames.Any()) return await _dbContext.Tags.ToListAsync();
+
+        return await _dbContext.Tags.Where(tag => tagNames.Contains(tag.Name)).ToListAsync();
     }
 }
